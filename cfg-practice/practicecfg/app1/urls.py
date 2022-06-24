f9d8bdafe350c028936466b765d887df8c9f8900
Kartikey Kabdwal <kartikeykabdwal86@gmail.com>
2022-06-24 11:00:37 +0530

    basic bakend

---
+++ cfg-practice/practicecfg/app1/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import include, path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name="index"),
+    path('login', views.login, name="login"),
+    path('register', views.register, name="register"),
+    path('dashboard', views.dashboard, name="dashboard"),
+
+]
--- cfg-practice/practicecfg/app1/views.py
+++ cfg-practice/practicecfg/app1/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def login(request):
+    return render(request, 'login.html')
+
+
+def register(request):
+    return render(request, 'register.html')
+
+
+def dashboard(request):
+    return render(request, 'dashboard.html')

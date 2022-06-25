e5744e642ece956123895e18ff8a5a5422218d15
Kartikey Kabdwal <kartikeykabdwal86@gmail.com>
2022-06-25 09:54:10 +0530

    Add files via upload

---
+++ final/myapp/app/urls.py
@@ -0,0 +1,12 @@
+from django.contrib.auth import views as auth_views
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('home/', views.home, name='home'),
+    path('index/', views.index, name='index'),
+    path('register/', views.register, name='register'),
+    path('', views.home, name="home"),
+]
